efd0fd9fa70dce7b9bd4ec09e8cfcb5d89b92a92
ssorrychoi <zzaesung17@gmail.com>
2020-11-24 23:46:01 +0900

    first commit

---
+++ lib/screen/add_memo_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddMemoScreen extends StatefulWidget {
+  @override
+  _AddMemoScreenState createState() => _AddMemoScreenState();
+}
+
+class _AddMemoScreenState extends State<AddMemoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Add Memo Screen'),
+    );
+  }
+}
